1c3e37467324b1a0693a7bb1fb0d890da20ff9b2
cıkay <muzaffer123cikay@gmail.com>
2021-03-19 21:07:35 +0300

    FeedBackForm top margin verildi

--- frontend/src/component/CenteredContainer.js
+++ frontend/src/component/CenteredContainer.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 export default function CenteredContainer({ children }) {
   return (
-    <Container className="d-flex align-items-center justify-content-center">
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ marginTop: '10px' }}
+    >
       <div className="w-100" style={{ maxWidth: '400px' }}>
         {children}
       </div>
